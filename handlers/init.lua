ba126db35bbbeee7f4d8d0917ed44d969830a1da
Miab0t <nycexs@gmail.com>
2019-10-04 00:41:51 -0400

    Add files via upload

---
+++ handlers/init.lua
@@ -0,0 +1,9 @@
+local handlerpath = justalocker.modpath .. "/handlers/"
+
+dofile(handlerpath.."nodeboxes.lua")
+
+dofile(handlerpath.."inventory.lua")
+
+dofile(handlerpath.."registration.lua")
+
+
